66d7d9a83adaccc7d63c54edc333c6f67b05a04b
Dan Brubaker Horst <dan.brubaker.horst@gmail.com>
2013-08-07 11:17:06 -0400

    Beginning Presenter class & tests

--- spec/controllers/hydra/object_viewer/application_controller_spec.rb
+++ spec/controllers/hydra/object_viewer/application_controller_spec.rb
@@ -1,26 +1,19 @@
 require 'spec_helper'
+require 'hydra/object_viewer/application_controller'
 
 module Hydra::ObjectViewer
 
-  class Presenter
-    def initialize(object)
-    end
-
-    def template_name
-      {text: 'Hello World'}
-    end
-  end
-
-  require 'hydra/object_viewer/application_controller'
-
   describe ApplicationController do
     let(:object_id) { '1234' }
+    let(:object) { double }
+    let(:presenter) { double({template_name: {text: 'Hello World'}}) }
+
     it 'renders a persisted object' do
-      subject.object_builder = lambda{|id, *args| Object.new}
+      subject.object_builder = lambda{|id, *args| object}
+      subject.presenter_builder = lambda{|object| presenter}
       get :show, id: object_id
       expect(response.status).to eq(200)
     end
-
   end
 
 end
