15c2e59ebb7a6947fae127ed9dac4443404075fa
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-07 16:10:05 -0400

    Developing markup & schema.org struct for viewer

---
+++ spec/views/hydra/object_viewer/application/show_spec.rb
@@ -0,0 +1,58 @@
+require 'spec_helper'
+
+describe 'hydra/object_viewer/application/show' do
+  def content_factory(options = {})
+    options.reverse_merge!(
+      description: %(<p id="lorem-id">Lorem Ipsum</p>).html_safe,
+      title: %(Hello World),
+      curation_concern: "Collection"
+    )
+    double(options)
+  end
+  def related_content_factory
+    double(
+      subject: content_factory,
+      annotation: content_factory
+    )
+  end
+
+  let(:primary_content) {
+    content_factory(related_contents: related_contents)
+  }
+
+  before(:each) do
+    view.stub(:presenter).and_return(primary_content)
+  end
+
+  describe 'with related contents' do
+    let(:related_contents) { [related_content_factory, related_content_factory] }
+    it 'renders the show page' do
+      render
+      rendered.should have_tag('.object-viewer') do
+        with_tag('.primary-content') do
+          with_tag('.title', text: primary_content.title)
+          with_tag('.curation-concern', text: primary_content.curation_concern)
+          with_tag('.description p#lorem-id')
+        end
+
+        with_tag('.related .related-content', count: related_contents.count)
+      end
+    end
+  end
+
+  describe 'with related contents' do
+    let(:related_contents) { [] }
+    it 'renders the show page' do
+      render
+      rendered.should have_tag('.object-viewer') do
+        with_tag('.primary-content') do
+          with_tag('.title', text: primary_content.title)
+          with_tag('.curation-concern', text: primary_content.curation_concern)
+          with_tag('.description p#lorem-id')
+        end
+
+        without_tag('.related')
+      end
+    end
+  end
+end
