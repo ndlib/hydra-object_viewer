bb4267cd000dea9351d3a3a944f58c56f0008c41
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-08 15:30:25 -0400

    Removing parse/syntax error

--- spec/lib/hydra/object_viewer_spec.rb
+++ spec/lib/hydra/object_viewer_spec.rb
@@ -13,9 +13,9 @@ describe Hydra::ObjectViewer do
       subject.stub(:converter).and_return(the_converter)
     end
 
-    it 'should convert an ID and context to a presenter_for' {
+    it 'should convert an ID and context to a presenter_for' do
       the_converter.should_receive(:call).with(findable_id, context).and_return(the_presenter)
       expect(subject.presenter_for(findable_id, context)).to eq(the_presenter)
-    }
+    end
   end
 end
