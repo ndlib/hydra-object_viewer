faa84a3134150af733261ce64552a41a828fe136
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-08 16:37:19 -0400

    Working on macro-like definitions for Presenter
    By using a macro to define a presenters base methods, we expose:
    
    1) What are the expected methods of a presenter
    2) Ensure that objects that are presented are wrapped accordingly
    
    By stating in the presenter class what the methods are, we can create
    Linters that validate each presenter.
    
    By ensuring that objects are properly wrapped, we in turn don't have
    to worry about exposing more of the object than we want.

--- lib/hydra/object_viewer/presenter.rb
+++ lib/hydra/object_viewer/presenter.rb
@@ -1,24 +1,44 @@
 class Hydra::ObjectViewer::Presenter
-  attr_reader :model, :context
-  def initialize(model, context)
-    @model, @context = model, context
+  attr_reader :model, :context, :container_model_context
+  def initialize(model, context, container_model_context = nil)
+    @model = model
+    @context = context
+    @container_model_context = container_model_context
   end
 
   delegate :title, :description, to: :model
+
   def classification
     model.classification
   rescue NoMethodError
     model.class.name.demodulize
   end
 
+  require 'morphine'
+  include Morphine
+  register :presenter_builder do
+    require 'hydra/object_viewer/presenter_builder'
+    lambda {|object, context|
+      Hydra::ObjectViewer::PresenterBuilder.new(object, context)
+    }
+  end
+
+  def self.related_contents(method_name)
+    define_method(:related_contents) do
+      send(method_name).collect {|object|
+        presenter_builder.call(object, context)
+      }
+    end
+  end
+
   def related_contents
     raise NotImplementedError,
-      "Method #related_contents should be overriden in child classes"
+      "Define via Class.related_contents(:method_name)"
   end
 
   def metadata_attributes
     raise NotImplementedError,
-      "Method #metadata_attributes should be overriden in child classes"
+      "Define via Class.metadata_attributes(:method_name)"
   end
 
 end
--- spec/lib/hydra/object_viewer/presenter_spec.rb
+++ spec/lib/hydra/object_viewer/presenter_spec.rb
@@ -1,14 +1,17 @@
 require 'spec_helper'
 
 describe Hydra::ObjectViewer::Presenter do
-  let(:the_model) {
+  def model_factory
     double(title: 'The Model Name', description: 'The Model Description')
-  }
+  end
+
+  let(:the_model) { model_factory }
   let(:the_context) { double }
   subject { Hydra::ObjectViewer::Presenter.new(the_model, the_context) }
 
   it {should respond_to(:model) }
   it {should respond_to(:context) }
+  it {should respond_to(:container_model_context) }
   its(:title) { should == the_model.title }
   its(:description) { should == the_model.description }
 
@@ -28,6 +31,34 @@ describe Hydra::ObjectViewer::Presenter do
     it "should fall back to the model's class name" do
       the_model.stub(:classification).and_raise(NoMethodError)
       subject.classification.should eq "Mock"
+    end
+  end
+
+  describe 'related_contents' do
+    before(:each) do
+      subject.presenter_builder = lambda {|*args|
+        Hydra::ObjectViewer::Presenter.new(*args)
+      }
+    end
+    let(:subclass) {
+
+      Class.new(Hydra::ObjectViewer::Presenter) {
+        def initialize(related_content_collection, *args)
+          @related_content_collection = related_content_collection
+          super(*args)
+        end
+        attr_reader :related_content_collection
+        related_contents :related_content_collection
+      }
+
+    }
+    let(:related_content) { [model_factory, model_factory] }
+    let(:subject) { subclass.new(related_content, the_model, the_context)}
+
+    it 'is set via a class method which in turn converts that collection to a collection of presenters' do
+      subject.related_contents.each_with_index do |content, i|
+        expect(content.model).to eq(related_content[i])
+      end
     end
   end
 end
