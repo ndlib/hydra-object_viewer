932eb853eeb08d1dc8757122aac6228d0e8a9d11
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-09 12:01:35 -0400

    Delegating rendering of primary content

--- spec/views/hydra/object_viewer/application/show_spec.rb
+++ spec/views/hydra/object_viewer/application/show_spec.rb
@@ -2,17 +2,13 @@ require 'spec_helper'
 
 describe 'hydra/object_viewer/application/show' do
   let(:the_presenter) {
-    double(
-      description: %(<p id="lorem-id">Lorem Ipsum</p>).html_safe,
-      title: %(Hello World),
-      classification: "Collection",
-      dom_id: rand(1000)
-    )
+    double(dom_id: rand(1000))
   }
 
   before(:each) do
     view.stub(:presenter).and_return(the_presenter)
     the_presenter.should_receive(:render_related_content).with(view).and_return("<div id='RENDERED_RELATED_CONTENT'></div>".html_safe)
+    the_presenter.should_receive(:render_primary_content).with(view).and_return("<div id='RENDERED_PRIMARY_CONTENT'></div>".html_safe)
   end
 
   describe 'with related contents' do
@@ -20,9 +16,7 @@ describe 'hydra/object_viewer/application/show' do
       render
       rendered.should have_tag('.object-viewer') do
         with_tag('.primary-content') do
-          with_tag('.title', text: the_presenter.title)
-          with_tag('.classification', text: the_presenter.classification)
-          with_tag('.description p#lorem-id')
+          with_tag('#RENDERED_PRIMARY_CONTENT')
         end
 
         with_tag('#RENDERED_RELATED_CONTENT')
