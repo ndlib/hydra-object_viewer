368b4239a06b187d58c39b29e0383b4a8fe26755
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-09 13:10:02 -0400

    Showing how a CustomPresenter is created

--- spec/features/render_a_presenter_spec.rb
+++ spec/features/render_a_presenter_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
+module Hydra::ObjectViewer::Presenters
+  class WatchPresenter < Hydra::ObjectViewer::Presenter
+    def classification
+      "Things That Tick"
+    end
+    def description
+      "<p class='line'>#{model.description}</p>".html_safe
+    end
+  end
+end
+
 describe 'Render A Presenter' do
   let(:object) { Watch.create(title: "My Title", description: "My Description") }
   let(:engine_mount_point) {
@@ -17,8 +28,8 @@ describe 'Render A Presenter' do
     expect(page).to have_tag(".object-viewer#watch_#{object.id}") do
       with_tag('.primary-content') do
         with_tag('.title', text: object.title )
-        with_tag('.classification', text: 'Watch' )
-        with_tag('.description')
+        with_tag('.classification', text: "Things That Tick" )
+        with_tag('.description .line', text: object.description)
       end
 
       with_tag('.related')
