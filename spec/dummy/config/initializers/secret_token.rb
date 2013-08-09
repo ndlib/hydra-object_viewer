056d9d846dc38318a270305971b35b0e831af8c7
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-09 10:55:45 -0400

    Removing spec dummy; This will be auto generated

--- spec/dummy/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-Dummy::Application.config.secret_token = 'a8cd8fc7fc5a0a289bf5ceec26583dc4ba891b1708bb4845626ce0d7457098dc63b7bdb90629cc185ae9da8ffb70814490d463fa90f34fdbfdc49e42de82386f'
--- spec/dummy/config/routes.rb
+++
@@ -1,4 +0,0 @@
-Rails.application.routes.draw do
-
-  mount Hydra::ObjectViewer::Engine => "/hydra/object_viewer"
-end
