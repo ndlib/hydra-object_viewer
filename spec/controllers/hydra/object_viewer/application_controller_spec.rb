2428c80d9110a0115ec4cc4236176bda6343f21b
Jeremy Friesen <jeremy.n.friesen@gmail.com>
2013-08-09 11:47:42 -0400

    Removing controller check for missing template

--- spec/controllers/hydra/object_viewer/application_controller_spec.rb
+++ spec/controllers/hydra/object_viewer/application_controller_spec.rb
@@ -6,7 +6,6 @@ module Hydra::ObjectViewer
   describe ApplicationController do
     let(:object_id) { '1234' }
     let(:object) { double }
-    let(:template_name) { 'show' }
     let(:presenter) { double({template_name: template_name}) }
 
     before(:each) do
@@ -17,17 +16,8 @@ module Hydra::ObjectViewer
       before(:each) do
         get :show, id: object_id
       end
-
-      its(:response) { should render_template(template_name) }
-
+      its(:response) { should be_successful }
       it { should respond_to(:presenter) }
-    end
-
-    describe "presenting an invalid template name" do
-      let(:template_name) {'this_view_does_not_exist'}
-      it 'renders a persisted object' do
-        expect {get :show, id: object_id}.to raise_error(ActionView::MissingTemplate)
-      end
     end
 
   end
